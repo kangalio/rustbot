efa42cfd26953f885678901fbd9dede05a9555b5
kangalioo <jannik.a.schaper@web.de>
2021-02-14 17:51:53 +0100

    Find members in ?ban case-insensitively

--- src/moderation.rs
+++ src/moderation.rs
@@ -50,7 +50,7 @@ except for mods",
     )
 }
 
-/// Look up a guild member by a string.
+/// Look up a guild member by a string, case-insensitively.
 ///
 /// The lookup strategy is as follows (in order):
 /// 1. Lookup by ID.
@@ -78,7 +78,7 @@ fn parse_member<'a>(members: &'a HashMap<UserId, Member>, string: &str) -> Optio
         let discrim = string[(pound_sign + 1)..].parse::<u16>().ok()?;
         members.values().find(|member| {
             let member = member.user.read();
-            member.discriminator == discrim && member.name == name
+            member.discriminator == discrim && member.name.eq_ignore_ascii_case(name)
         })
     };
 
@@ -89,9 +89,10 @@ fn parse_member<'a>(members: &'a HashMap<UserId, Member>, string: &str) -> Optio
     };
 
     let lookup_by_nickname = || {
-        members
-            .values()
-            .find(|member| member.nick.as_deref() == Some(string))
+        members.values().find(|member| match &member.nick {
+            Some(nick) => nick.eq_ignore_ascii_case(string),
+            None => false,
+        })
     };
 
     lookup_by_id()
