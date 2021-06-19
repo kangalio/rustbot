7cd0a138c376aa8c9c0fd70a12557b329626b7e2
kangalioo <jannik.a.schaper@web.de>
2021-06-19 23:23:42 +0200

    Allow server owner to use ?register
    Previously, it was hardcoded to allow only me (kangalioo)

--- src/misc.rs
+++ src/misc.rs
@@ -26,23 +26,28 @@ You can edit your message to the bot and the bot will edit its response.";
     Ok(())
 }
 
-pub async fn is_owner(ctx: crate::PrefixContext<'_>) -> Result<bool, Error> {
-    Ok(ctx.msg.author.id.0 == 472029906943868929)
-}
-
 /// Register slash commands in this guild or globally
 ///
 /// Run with no arguments to register in guild, run with argument "global" to register globally.
-#[poise::command(check = "is_owner", hide_in_help)]
+#[poise::command(hide_in_help)]
 pub async fn register(ctx: PrefixContext<'_>, #[flag] global: bool) -> Result<(), Error> {
-    let guild_id = ctx.msg.guild_id.ok_or("Must be called in guild")?;
+    let guild = ctx
+        .msg
+        .guild(ctx.discord)
+        .await
+        .ok_or("Must be called in guild")?;
+
+    if ctx.msg.author.id != guild.owner_id {
+        return Err("Can only be used by server owner".into());
+    }
+
     let commands = &ctx.framework.options().slash_options.commands;
     poise::say_prefix_reply(ctx, format!("Registering {} commands...", commands.len())).await?;
     for cmd in commands {
         if global {
             cmd.create_global(&ctx.discord.http).await?;
         } else {
-            cmd.create_in_guild(&ctx.discord.http, guild_id).await?;
+            cmd.create_in_guild(&ctx.discord.http, guild.id).await?;
         }
     }
     poise::say_prefix_reply(ctx, "Done!".to_owned()).await?;
