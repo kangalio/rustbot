6a04d191987c6194c9a71dd3e2b14e2f9f4de498
kangalioo <jannik.a.schaper@web.de>
2021-09-13 16:41:29 +0200

    Update poise

--- src/misc.rs
+++ src/misc.rs
@@ -23,11 +23,11 @@ pub async fn help(
     let bottom_text = "You can still use all commands with `?`, even if it says `/` above.
 Type ?help command for more info on a command.
 You can edit your message to the bot and the bot will edit its response.";
-    poise::defaults::help(
+    poise::samples::help(
         ctx,
         command.as_deref(),
         bottom_text,
-        poise::defaults::HelpResponseMode::Ephemeral,
+        poise::samples::HelpResponseMode::Ephemeral,
     )
     .await?;
     Ok(())
@@ -38,7 +38,7 @@ You can edit your message to the bot and the bot will edit its response.";
 /// Run with no arguments to register in guild, run with argument "global" to register globally.
 #[poise::command(prefix_command, hide_in_help)]
 pub async fn register(ctx: PrefixContext<'_>, #[flag] global: bool) -> Result<(), Error> {
-    poise::defaults::register_application_commands(ctx.into(), global).await?;
+    poise::samples::register_application_commands(ctx.into(), global).await?;
 
     Ok(())
 }
@@ -67,57 +67,7 @@ pub async fn uptime(ctx: Context<'_>) -> Result<(), Error> {
 /// List servers of which the bot is a member of
 #[poise::command(slash_command, prefix_command, track_edits, hide_in_help)]
 pub async fn servers(ctx: Context<'_>) -> Result<(), Error> {
-    let mut show_private_guilds = false;
-    if let Context::Application(_) = ctx {
-        if let Ok(app) = ctx.discord().http.get_current_application_info().await {
-            if app.owner.id == ctx.author().id {
-                show_private_guilds = true;
-            }
-        }
-    }
-
-    struct Guild {
-        name: String,
-        num_members: u64,
-        is_public: bool,
-    }
-
-    let guild_ids = ctx.discord().cache.guilds();
-    let mut guilds = guild_ids
-        .into_iter()
-        .filter_map(|guild_id| {
-            ctx.discord().cache.guild_field(guild_id, |guild| Guild {
-                name: guild.name.clone(),
-                num_members: guild.member_count,
-                is_public: guild.features.iter().any(|x| x == "DISCOVERABLE"),
-            })
-        })
-        .collect::<Vec<_>>();
-    guilds.sort_by_key(|guild| u64::MAX - guild.num_members); // sort descending
-
-    let mut num_private_guilds = 0;
-    let mut num_private_guild_members = 0;
-    let mut response = format!("I am currently in {} servers!\n", guilds.len());
-    for guild in guilds {
-        if guild.is_public || show_private_guilds {
-            response += &format!("- **{}** ({} members)\n", guild.name, guild.num_members);
-        } else {
-            num_private_guilds += 1;
-            num_private_guild_members += guild.num_members;
-        }
-    }
-    if num_private_guilds > 0 {
-        response += &format!(
-            "- [{} private servers with {} members total]\n",
-            num_private_guilds, num_private_guild_members
-        );
-    }
-
-    if show_private_guilds {
-        response += "\n_Showing private guilds because you are the bot owner_";
-    }
-
-    poise::send_reply(ctx, |f| f.content(response).ephemeral(show_private_guilds)).await?;
+    poise::samples::servers(ctx).await?;
 
     Ok(())
 }
