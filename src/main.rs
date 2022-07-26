9bc27f3cf2058de53fcc05155247e37968cb5357
kangalioo <jannik.a.schaper@web.de>
2022-07-27 00:08:35 +0200

    Add joke ?get command
    ?get a better computer

--- src/main.rs
+++ src/main.rs
@@ -184,6 +184,7 @@ async fn app() -> Result<(), Error> {
             misc::uptime(),
             misc::servers(),
             misc::revision(),
+            misc::get(),
         ],
         prefix_options: poise::PrefixFrameworkOptions {
             prefix: Some("?".into()),
