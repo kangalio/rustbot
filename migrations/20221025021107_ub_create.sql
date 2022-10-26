94380f054cdfb66ee74b92f6d62f6e2ffce48b3d
Scott Fleener <scfleener@gmail.com>
2022-10-25 22:58:33 -0400

    Document SQL migration table

--- migrations/20221025021107_ub_create.sql
+++ migrations/20221025021107_ub_create.sql
@@ -1,7 +1,10 @@
--- Add migration script here
 CREATE TABLE ub (
+    -- The previous time UB was invoked, will be in RFC 3339 format in UTC
     time TEXT NOT NULL,
+    -- Channel ID where it was invoked. Should always be BEGINNER_CHANNEL_ID set from env,
+    -- but this allows the channel to be updated.
     channel INTEGER NOT NULL,
+    -- The kind of UB invoked
     kind TEXT NOT NULL,
     UNIQUE (channel, kind)
 );
